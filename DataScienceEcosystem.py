3ba1e3c7f6bc9d7dec7b976e20a2fcf8c24125d7
kawsarp <130893214+kawsarp@users.noreply.github.com>
2023-04-19 21:52:03 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# <h2>Author</h2>
+
+# Kawsar Salam
+
+# <b>Objectives:</b>
+
+# *  Python
+# *  R
+# *  Scala
+# *  Java
+# *  Julia
+# *  SQL
+
+# Some of the popular languages that Data Scientists use are:
+
+# * List popular languages for Data Science
+
+# 1.  Python
+# 2.  R
+# 3.  Scala
+# 4.  Java
+# 4.  Julia
+# 5.  SQL 
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# *  List the scientific computing libraries in Python, e.g., pandas, Numpy
+# *  List the visualisation libraries in Python, e.g., matplotlib, seaborn
+# *  List the high-level machine learning and deep learning in Python, e.g., tensorFlow, PyTorch
+# 
+
+# <table ><tr><th >Data Science Tools <th><tr>
+# <tr><td> MySQL <td><tr>
+#   <tr><td> Jupyter Notebook <td><tr>  
+#       <tr><td> RStudio <td><tr>  
+#     <tr><td> MongoDB <td><tr>  
+#     <tr><td> Hadoop File System <td><tr> 
+#     <table>
+
+# <h3> Below are a few examples of evaluating arithmetic expressions in Python:
+
+# In[7]:
+
+
+a=4
+b=7
+a+b
+
+
+# In[8]:
+
+
+a*b
+
+
+# In[9]:
+
+
+b/a
+
+
+# In[10]:
+
+
+x=8
+y=8
+x==y
+
+
+# In[11]:
+
+
+x<=y
+
+
+# In[12]:
+
+
+x>=y
+
+
+# In[13]:
+
+
+x*10+y
+
+
+# <i>This a simple arithmetic expression to mutiply then add integers </i>
+
+# In[1]:
+
+
+(3*4)+5
+
+
+# <i>This will convert 200 minutes to hours by diving by 60</i>
+
+# In[4]:
+
+
+200/60
+
+
+# <h2> Author</h2>
+
+# Kawsar Salam
+
+# In[ ]:
+
+
+
+
